904d91510b29302b4e18f80067efda63bf69cc79
Markossssss <marcospachecoiaw.2018@gmail.com>
2019-12-10 17:28:09 -0300

    Adicionadas fontes e bg

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,54 @@ void main(){
   );
 }
 
-class Home extends StatefulWidget{
-  @override
-  _HomeState  createState()=>_HomeState();
-}
-class _HomeState extends State<Home> {
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+const ThemeColor = const Color(0xFF58BCFF);
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(100.0),
+        child:AppBar(
+          backgroundColor: ThemeColor,
+          centerTitle: true,
+          title: Padding(padding: EdgeInsets.only(top: 30.0),
+              child: Text('Chá de Bebê!',
+                style: (TextStyle(
+                color: Colors.white,
+                fontFamily: 'Pacifico',
+                fontSize: 25.0
+                )
+              ),
+            ),
+          ),
+        ),
+      ),
+
+      body: Stack(
+        children: <Widget>[
+          Image.asset(
+            "images/bg/bgApp.png",
+            fit: BoxFit.cover,
+            height: 900.0,
+          ),
+          Column(
+            children: <Widget>[
+              Container(
+                child: Text('Olá mundo!',
+                ),
+              ),
+            ],
+          ),
+        ],
+      )
+    );
   }
 }
