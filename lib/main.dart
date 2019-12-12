38d495706b28919a1c9d29156dc32bcab95161e2
Markossssss <marcospachecoiaw.2018@gmail.com>
2019-12-11 21:01:49 -0300

    Containers funcionando

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(
-    home: Home(),)
-  );
+    home: Home(),
+  ));
 }
 
 class Home extends StatelessWidget {
@@ -15,71 +15,76 @@ class Home extends StatelessWidget {
     );
   }
 }
+
 const ThemeColor = const Color(0xFF58BCFF);
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(100.0),
-        child:AppBar(
-          backgroundColor: ThemeColor,
-          centerTitle: true,
-          title: Padding(padding: EdgeInsets.only(top: 30.0),
-              child: Text('Chá de Bebê!',
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(100.0),
+          child: AppBar(
+            backgroundColor: ThemeColor,
+            centerTitle: true,
+            title: Padding(
+              padding: EdgeInsets.only(top: 20.0),
+              child: Text(
+                'Chá de Bebê!',
                 style: (TextStyle(
-                color: Colors.white,
-                fontFamily: 'Pacifico',
-                fontSize: 22.0
-                )
+                    color: Colors.white,
+                    fontFamily: 'Pacifico',
+                    fontSize: 28.0)),
               ),
             ),
           ),
         ),
-      ),
-
-      body: Stack(
-        children: <Widget>[
-          Image.asset(
-            "images/bg/bgApp.png",
-            fit: BoxFit.cover,
-            alignment: Alignment.center,
-            height: 900.0,
-            width: 500.0,
-          ),
-          Column(
-            children: <Widget>[
-              Padding(padding: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.9),
-                    borderRadius: BorderRadiusDirectional.only(
-                        bottomStart: Radius.circular(10.0),
-                        bottomEnd: Radius.circular(10.0)
+        body: Stack(
+          children: <Widget>[
+            Image.asset(
+              "images/bg/bgApp.png",
+              fit: BoxFit.cover,
+              alignment: Alignment.center,
+              height: 900.0,
+              width: 500.0,
+            ),
+            Padding(
+                padding: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 20.0),
+                child: Stack(
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Container(
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                  //Inputs de dados
+                                  height: 100.0,
+                                  width: 100.0,
+                                  color: Colors.red),
+                              Container(
+                                  // output de dados
+                                  height: 100.0,
+                                  width: 100.0,
+                                  color: Colors.green)
+                            ],
+                          ),
+                          height: 539.0,
+                          width: 1000.0,
+                          decoration: BoxDecoration(
+                            color: Colors.white.withOpacity(0.9),
+                            borderRadius: BorderRadiusDirectional.only(
+                                bottomStart: Radius.circular(10.0),
+                                bottomEnd: Radius.circular(10.0)),
+                          ),
+                        )
+                      ],
                     )
-                  ),
-                  alignment: Alignment.topCenter,
-                  width: 500,
-                  height: 540,
-                  child: Container(
-                    alignment: Alignment.center,
-                    color: Colors.black,
-                    width: 500,
-                    height: 205,
-                    child: Text('Fala rapaze da quebrada!',
-                      textAlign: TextAlign.center,
-                      style: (TextStyle(
-                        fontFamily: 'Pacifico',
-                        color: Colors.white,
-                      )),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ],
-      )
+                  ],
+                )
+            )
+          ],
+        )
     );
   }
 }
